8b15a18ab940825de83be1f5604e6bf445fe427b
Abhinav Raj <abhinavshawarn@gmail.com>
2021-09-06 04:21:20 +0800

    Add files via upload

---
+++ Testing Hardware backend.py
@@ -0,0 +1,17 @@
+import strangeworks.qiskit
+import qiskit
+
+qc = qiskit.QuantumCircuit(4, 4)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0, 1], [0, 1])
+
+# uncomment the next line if you have more than one account
+# backend = strangeworks.qiskit.get_backend("ibmqx2", account_id="ibm")
+backend = strangeworks.qiskit.get_backend("ibmq_bogota")
+
+job = qiskit.execute(qc, backend, shots=10)
+result = job.result()
+print("result:", result)
+counts = result.get_counts()
+print("counts:", counts)
